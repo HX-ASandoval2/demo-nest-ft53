26f25fc2895bf4fd65d12473dbd457ad1313f01c
HX-ASandoval2 <auri.sandoval@gmail.com>
2024-09-25 14:50:09 -0300

    fix config typeorm

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DateAdderInterceptor } from './interceptors/date-adder.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(loggerGlobal);
